3c22554f38378d44e15963d9f5732940b185e354
Jetty-Lee <jetty0188@gmail.com>
2025-06-16 11:24:36 +0900

    CHAM-55 feat: swagger에서 음성 파일 업로드할 수 있게 수정

--- src/main/java/com/example/demo/voice/controller/VoiceController.java
+++ src/main/java/com/example/demo/voice/controller/VoiceController.java
@@ -4,7 +4,13 @@ import com.example.demo.voice.dto.TranscribedTextResponse;
 import com.example.demo.voice.dto.TtsLogRequest;
 import com.example.demo.voice.dto.TtsLogResponse;
 import com.example.demo.voice.service.VoiceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,9 +25,15 @@ public class VoiceController {
     private final VoiceService voiceService;
 
     // 음성 파일 업로드 및 STT 처리
-    @PostMapping("/upload")
+    @Operation(summary = "음성 업로드", description = "음성 파일 업로드 및 STT 처리")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공")
+    })
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<TranscribedTextResponse> uploadAudio(
+            @Parameter(description = "음성 파일", content = @Content(mediaType = "multipart/form-data"))
             @RequestParam("audio_file") MultipartFile audioFile,
+            @Parameter(description = "세션 ID")
             @RequestParam("session_id") Long sessionId
 //            @RequestHeader("Authorization") String authHeader
     ) throws IOException {

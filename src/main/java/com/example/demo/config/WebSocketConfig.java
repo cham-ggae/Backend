ea61332d16fffb5b3a933ecf645866619f047263
choi-hi <gmltmdsp@hanmail.net>
2025-06-20 15:03:25 +0900

    CHAM-75 refactor: 소켓 기능이 물주기에서 전체 활동으로 수정

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.config;
 
-import com.example.demo.plant.websocket.WaterWebSocketHandler;
+import com.example.demo.plant.websocket.PlantWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -12,11 +12,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final WaterWebSocketHandler waterWebSocketHandler;
+    private final PlantWebSocketHandler plantWebSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(waterWebSocketHandler, "/ws/water")
+        registry.addHandler(plantWebSocketHandler, "/ws/plant")
                 .setAllowedOrigins("http://localhost:3000", "https://localhost:3000");
     }
 }
